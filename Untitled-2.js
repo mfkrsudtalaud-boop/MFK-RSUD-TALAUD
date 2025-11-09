7f378fa5c5dc51fb4e7421f3de34ceb296de2f98
mfkrsudtalaud-boop <mfkrsudtalaud@gmail.com>
2025-11-09 13:07:52 +0800

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,8 @@
+// Ubah PIN
+const CORRECT_PIN = 'PIN_BARU';
+
+// Tambah lokasi baru
+const newLocations = ['LOKASI_BARU'];
+
+// Modifikasi algoritma prioritas
+// Edit fungsi analyzePriority() di index.html
